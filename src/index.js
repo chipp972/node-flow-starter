ef5b539d95adcd9ef36b514fb01213d833aa6ee0
Pierre-Charles Nicolas <pierrecharles.nicolas@gmail.com>
2018-02-07 13:01:26 +0100

    config update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@
 
 export default function(name: string, delay: number = 2000): Promise<string> {
   return new Promise((resolve, reject) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay)
+    setTimeout(() => resolve(`Hello, ${name}`), delay),
   );
 }
